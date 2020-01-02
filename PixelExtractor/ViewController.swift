cd2a6c29d8e4b5ea1f84f489a96bf0fefe787856
MB <monsterpit104@gmail.com>
2020-01-02 21:47:33 +0530

    Initial Commit

---
+++ PixelExtractor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PixelExtractor
+//
+//  Created by Mojave on 02/01/20.
+//  Copyright © 2020 Mojave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
